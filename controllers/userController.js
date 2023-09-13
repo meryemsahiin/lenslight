ad0efbc35389b2d3ad064a0210c224a1041e071d
Meryem Sahin <sahinmeryem620@gmail.com>
2023-09-13 16:57:20 +0300

    lesson 12

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 import User from "../models/userModel.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken"
 
 const createUser = async (req, res) => {
   try {
@@ -34,7 +35,10 @@ const loginUser = async (req, res) => {
    }
 
    if (same) {
-    res.status(200).send("You are loggend in")
+    res.status(200).json({
+      user,
+      token:createToken(user._id)
+    })
    } else {
     res.status(401).json({
       succeded: false,
@@ -48,5 +52,9 @@ const loginUser = async (req, res) => {
     });
   }
 };
+
+const createToken = (userId) => {
+  return jwt.sign({userId}, process.env.JWT_SECRET, {expiresIn: '1d'})
+}
 
 export { createUser, loginUser};
