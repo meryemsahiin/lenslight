2e6c7a61dd40ec7a9d12c2521a7a8f902b9f0310
Meryem Sahin <sahinmeryem620@gmail.com>
2023-09-14 16:46:57 +0300

    lesson 18

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -7,6 +7,14 @@ const router = express.Router();
 
 router.route('/register').post(userController.createUser);
 router.route('/login').post(userController.loginUser);
-router.route('/dashboard').get(authMiddleware.authenticateToken, userController.getDashboardPage);
+router
+    .route('/dashboard')
+    .get(authMiddleware.authenticateToken, userController.getDashboardPage);
+router
+    .route('/')
+    .get(authMiddleware.authenticateToken, userController.getAllUsers);
+router
+    .route('/:id')
+    .get(authMiddleware.authenticateToken, userController.getUser)
 
 export default router;
